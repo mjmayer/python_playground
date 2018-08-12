d79012c9e7058f4b90567b862a34d5d72ab05be2
Michael Mayer <mjmayer@gmail.com>
2018-08-12 08:57:45 -0700

    Event creation working

--- calendar_api.py
+++ calendar_api.py
@@ -2,15 +2,16 @@ import requests
 import requests_oauthlib
 import json
 import pprint
+import datetime
 
 #import client oauth
-authfile = json.loads(open("client_id.json").read())
+authfile = json.loads(open('client_id.json').read())['web']
 
 #setup auth variables
-CLIENT_ID = ''
-CLIENT_SECRET = ''
-REDIRECT_URI = 'https://www.mayerinformatics.com/'
-TOKEN_URL = "https://accounts.google.com/o/oauth2/token"
+CLIENT_ID = authfile['client_id']
+CLIENT_SECRET = authfile['client_secret']
+REDIRECT_URI = authfile['redirect_uris'][0]
+TOKEN_URL = authfile['token_uri']
 TOKENFILE = "token"
 GC_BASEURL = 'https://www.googleapis.com/calendar/v3'
 
@@ -27,7 +28,7 @@ def gettoken (client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=RED
     print('Please go to %s and authorize access.' % authorization_url)
     
     # Get the authorization verifier code from the callback url
-    authorization_response = raw_input('Enter the full callback URL')
+    authorization_response = input('Enter the full callback URL')
     
     token = oauth.fetch_token(
             'https://accounts.google.com/o/oauth2/token',
@@ -59,6 +60,7 @@ if tokenfromfile is None:
     tokenfromfile = readtoken(tokenfile=TOKENFILE)
 
 #refresh token
+# TODO Refresh is not working
 extra = {
     'client_id': CLIENT_ID,
     'client_secret': CLIENT_SECRET
@@ -67,10 +69,19 @@ client = requests_oauthlib.OAuth2Session(CLIENT_ID, token=tokenfromfile, auto_re
                        auto_refresh_kwargs=extra, token_updater=tokensaver)
 
 callist = client.get(GC_BASEURL + '/users/me/calendarList')
-callist = json.loads(callist.content)
+callist = json.loads(callist.content.decode())
 for c in callist['items']:
     if (c['summary']) == "Todoist":
        todoistcal = c
+pprint.pprint(todoistcal)
+
+starttime = datetime.datetime.now()
+enddtime = (starttime + datetime.timedelta(hours=1)).isoformat('T') + 'Z'
+body = { "summary": "My event", "description": "Foo", "end": { "dateTime": enddtime }, "start": { "dateTime" : starttime.isoformat('T') + 'Z' }}
+body_json = json.dumps(body)
+print(body_json)
+event = client.post(GC_BASEURL + '/calendars/' + todoistcal['id'] + '/events', json=body)
+print("Event Status: {} Event Content: {}, Event URL: {}".format(event.status_code, event.content, event.url))
 
 
 #c = client.get(GC_BASEURL + '/calendars/primary')
